b042816bf40a45cd30b69c128aeaeaf4ef4234a2
Karan Pratap Singh <synester.gates4@gmail.com>
2019-06-21 21:13:56 +0530

    added prisma functions and datamodel relations

--- src/prisma.js
+++ src/prisma.js
@@ -2,6 +2,48 @@ import { Prisma } from 'prisma-binding';
 
 const prisma = new Prisma({
     typeDefs: 'src/generated/prisma.graphql',
-    endpoint: 'localhost:4466'
+    endpoint: 'http://localhost:4466'
 });
+
+const createPostForUser = async (authorId, data) => {
+
+    const userExists = await prisma.exists.User({ id: authorId });
+
+    if (!userExists) throw new Error('User not found');
+
+    const post = await prisma.mutation.createPost({
+        data: {
+            ...data,
+            author: {
+                connect: {
+                    id: authorId
+                }
+            }
+        }
+    }`{ id }`);
+
+    const user = await prisma.query.user({
+        where: {
+            id: authorId
+        }
+    }, '{ author { id name email posts { id title published } } }');
+
+    return post.author;
+};
+
+const updatePostForUser = async (postId, data) => {
+
+    const postExists = await prisma.exists.Post({ id: postId });
+
+    if (!postExists) throw new Error('Post not found');
+
+    const post = await prisma.mutation.updatePost({
+        where: {
+            id: postId
+        },
+        data
+    }, `{ author { id name email posts { id title published } } }`);
+
+    return post.author;
+};
 
